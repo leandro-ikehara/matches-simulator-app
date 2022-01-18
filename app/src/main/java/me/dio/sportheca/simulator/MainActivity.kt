d926c78b39e54831ab0514247440e36a6bf9f109
Leandro Ikehara <leandro.ikehara@gmail.com>
2022-01-18 15:21:45 -0300

    Delete MainActivity em Kotlin

--- app/src/main/java/me/dio/sportheca/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/sportheca/simulator/MainActivity.kt
@@ -1,11 +1,22 @@
 package me.dio.sportheca.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.sportheca.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        setContentView(R.layout.activity_main)
+//        val tvHello = findViewById<TextView>(R.id.tvHello)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
